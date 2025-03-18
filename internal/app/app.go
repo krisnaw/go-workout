6098f1ed85cb75554a452af8289bdd62098e229b
krisna <krisna.w2010@gmail.com>
2025-03-18 10:13:15 +0800

    Add Application struct and basic HealthCheck endpoint
    This commit introduces an `Application` struct to encapsulate key components like the logger and handlers. It also adds a simple `HealthCheck` endpoint to verify service availability. These changes lay the foundation for the application's structure and future extensions.

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,34 @@
 package app
+
+import (
+	"fmt"
+	"gofun/internal/api"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Application struct {
+	Logger         *log.Logger
+	WorkoutHandler *api.WorkoutHandler
+}
+
+func NewApplication() (*Application, error) {
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// our store will go here
+
+	// our handler will go here
+	workoutHandler := api.NewWorkoutHandler()
+
+	app := &Application{
+		Logger:         logger,
+		WorkoutHandler: workoutHandler,
+	}
+
+	return app, nil
+}
+
+func (a *Application) HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status is available\n")
+}
